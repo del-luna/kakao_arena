72eaea23492e3956badf658718c69187a785a39d
Jaeheon <jaeheon.dev@gmail.com>
2020-07-17 22:13:56 +0900

    modify

--- ALS.py
+++ ALS.py
@@ -20,11 +20,11 @@ def als_run(data, n_items, train, test, idx_to_sid, idx_to_tag):
     tag_ret = []
     from tqdm.auto import tqdm
     for u in tqdm(range(test.shape[0])):
-        item_rec = item_model.recommend(u, item_rec_csr, N=1000)
+        item_rec = item_model.recommend(u, item_rec_csr, N=5000)
         item_rec = [idx_to_sid[x[0]] for x in item_rec]
-        tag_rec = tag_model.recommend(u, tag_rec_csr, N=10) #candiate 용이라 N = 100으로 수정해야함
+        tag_rec = tag_model.recommend(u, tag_rec_csr, N=100)
         tag_rec = [idx_to_tag[x[0]] for x in tag_rec if x[0] in idx_to_tag]
         item_ret.append(item_rec)
         tag_ret.append(tag_rec)
    
-    return item_ret, tag_ret
+    return als_model, item_model, tag_model
